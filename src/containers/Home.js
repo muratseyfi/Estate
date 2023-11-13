f55b954e2a6b8f868e18507158c6ae9b9b91a003
muratseyfi <muratseyfitr@gmail.com>
2023-11-13 13:02:45 +0300

    Home Page and build folder

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,71 +5,68 @@ import Listings from '../components/Listings';
 import Pagination from '../components/Pagination';
 
 const Home = () => {
-  
-  const [listings, setListings] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [linstingsPerPage, setListingsPerPage] = useState(3);
-  const [active, setActive] = useState(1);
+    const [listings, setListings] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [listingsPerPage, setListingsPerPage] = useState(3);
+    const [active, setActive] = useState(1);
 
-  const indexOfLastListing = currentPage * linstingsPerPage;
-  const indexOfFirsListing = indexOfLastListing - linstingsPerPage;
-  const currentListings = listings.slice(indexOfFirsListing, indexOfLastListing);
+    const indexOfLastListing = currentPage * listingsPerPage;
+    const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+    const currentListings = listings.slice(indexOfFirstListing, indexOfLastListing);
 
-  const visitPage = (page) => {
-    setCurrentPage(page);
-    setActive(page);
-  };
+    const visitPage = (page) => {
+        setCurrentPage(page);
+        setActive(page);
+    };
 
-  const previous_number = () => {
-    if(currentPage !== 1) {
-      setCurrentPage(currentPage - 1);
-      setActive(currentPage - 1);
-    }
-  };
+    const previous_number = () => {
+        if (currentPage !== 1) {
+            setCurrentPage(currentPage-1);
+            setActive(currentPage-1);
+        }
+    };
 
-  const next_number = () => {
-    if(currentPage !== Math.ceil(listings.length/3)) {
-      setCurrentPage(currentPage + 1);
-      setActive(currentPage + 1);
-    }
-  };
-  
+    const next_number = () => {
+        if (currentPage !== Math.ceil(listings.length/3)) {
+            setCurrentPage(currentPage+1);
+            setActive(currentPage+1);
+        }
+    };
 
-  return (
-    <main className='home'>
-      <Helmet>
-        <title>realest Estate - Home</title>
-        <meta 
-          name= 'description'
-          content= 'Realest Estate Home Page'
-        />
-      </Helmet>
-      <section className='home__form'>
-        <ListingForm setListings={setListings} />
-      </section>
-      <section className='home__listings'>
-        <Listings listings={currentListings} />
-      </section>
-      <section className='home__pagination'>
-        <div className='row'>
-          {
-            listings.length !== 0 ? (
-              <Pagination 
-                itemsPerPage = {linstingsPerPage}
-                count = {listings.length}
-                visitPage = {visitPage}
-                previous = {previous_number}
-                next = {next_number}
-                active = {active}
-                setActive = {setActive}
-              />
-            ) : null
-          }
-        </div>
-      </section>
-    </main>
-  );
-
+    return (
+        <main className='home'>
+            <Helmet>
+                <title>Realest Estate - Home</title>
+                <meta
+                    name='description'
+                    content='Realest Estate Home Page'
+                />
+            </Helmet>
+            <section className='home__form'>
+                <ListingForm setListings={setListings} />
+            </section>
+            <section className='home__listings'>
+                <Listings listings={currentListings} />
+            </section>
+            <section className='home__pagination'>
+                <div className='row'>
+                    {
+                        listings.length !== 0 ? (
+                            <Pagination
+                                itemsPerPage={listingsPerPage}
+                                count={listings.length}
+                                visitPage={visitPage}
+                                previous={previous_number}
+                                next={next_number}
+                                active={active}
+                                setActive={setActive}
+                            />
+                        ) : null
+                    }
+                </div>
+            </section>
+        </main>
+    );
 };
 
 export default Home;
